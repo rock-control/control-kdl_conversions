3fc9634686853971f02212c8bb76b8a28fa21d61
dmronga <dennis.mronga@dfki.de>
2013-10-08 12:18:45 +0200

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,20 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+#include <iostream>
+
+namespace kdl_conversions
+{
+	class DummyClass
+	{
+		public: 
+			/**
+			* Print a welcome to stdout
+			* \return nothing
+			*/
+			void welcome();
+	};
+
+} // end namespace kdl_conversions
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <kdl_conversions/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+	kdl_conversions::DummyClass dummyClass;
+	dummyClass.welcome();
+
+	return 0;
+}
